6e260a057c846f9da8f5bab926221b4a0d4353ff
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-05-28 15:35:07 -0700

    declated variables to enter number and operator

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('I am writing typescript');
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = readline_sync_1.question('Enter first number:\n');
+    var operator = readline_sync_1.question('Enter operator:\n');
+    var secondStr = readline_sync_1.question('Enter second number:\n');
+    console.log(firstStr, operator, secondStr);
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('I am writing typescript')
+import { question } from 'readline-sync'
+
+function main(): void 
+{
+    const firstStr: string = question('Enter first number:\n')
+    const operator: string = question('Enter operator:\n')
+    const secondStr: string = question('Enter second number:\n')
+
+    console.log(firstStr, operator, secondStr)
+}
+
+main()
